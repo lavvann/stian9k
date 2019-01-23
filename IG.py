d55587b61f59d749cfdd8ff4d0399e9ddeffdffd
lavvann <shafslund@gmail.com>
2019-01-23 15:49:44 +0100

    Corrected trade gain calculation

--- IG.py
+++ IG.py
@@ -11,6 +11,7 @@ sg = 0.0                # spot gold price EUR
 m = 0.0                 # margin (deposit for each trade)
 sim_finished = False    # BOOL true when test set is completed
 goal = 1.003            # default training gain factor 
+spread = 0.3            # default IG spread $
 
 
 """ ------- MAIN --------- """
@@ -34,6 +35,7 @@ while not sim_finished:
     trading_l = False
     trading_s = False
     for i in range(0, len(df.index)-1, 1):
+        sg = df.iloc[i, 1] # current spot gold
         # long
         If df.iloc[i, 3]
             if not trading_l:
@@ -44,6 +46,7 @@ while not sim_finished:
                 else:
                     position = int(bank/m1)
                     if position == 0: break
+                l_start = sg
                 trading_l = True
             target = df.iloc[i, 1] * goal   # Update target
                   
@@ -57,6 +60,7 @@ while not sim_finished:
                 else:
                     position = int(bank/m1)
                     if position == 0: break
+                s_start = sg
                 trading_s = True
             target = df.iloc[i, 1] * (1/goal)  # Update target
             
@@ -64,13 +68,13 @@ while not sim_finished:
         if trading_l:   # end long
             if df.iloc[i, 1] >= target:
                 position = 0
-                bank += std_pos
+                bank += std_pos*(l_start-sg) - (std_pos*spread)  # Trade gain EUR
                 trading_l = False
                 target = 0
          if trading_s:  # end short
             if df.iloc[i, 1] <= target:
                 position = 0
-                bank += std_pos
+                bank += std_pos*(sg-s_start) - (std_pos*spread)  # Trade gain EUR
                 trading_s = False
                 target = 0
         
