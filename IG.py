47b60ffe1cfab67467ab66d198d9200d6c46e5bc
lavvann <shafslund@gmail.com>
2019-01-24 15:36:45 +0100

    Troubleshooting negative trades, IG.py

--- IG.py
+++ IG.py
@@ -19,11 +19,19 @@ std_pos = 100           # default trade amount
 
 def plot_result(y, span=1000, start=0):
     # Plotting:
+    start = 336500
     fig, ax = plt.subplots()
+    # axis y1
     x = y[start:(start + span), 0]
     y1 = y[start:(start + span), 1]
+    ax.plot(x, y1)
+    # axis y2
+    ax2 = ax.twinx()  # instantiate a second axes that shares the same x-axis
+    ax2.set_ylabel('EUR', color='r')  # we already handled the x-label with ax1
     y2 = y[start:(start + span), 5]
-    ax.plot(x, y1, y2)
+    ax2.plot(x, y2, 'r')
+    ax2.tick_params(axis='y', labelcolor='r')
+    
     plt.xticks(rotation=80)  # rotate x ticks from horizontal
     plt.tight_layout()  # fit everything into window
     plt.grid(b=True, which='major', color='k', linestyle='--')  # Set grid
@@ -34,11 +42,11 @@ def plot_result(y, span=1000, start=0):
 
     for r in y[start:(start+span)]:
         if r[2]:
-            plt.axvline(x=r[0], color='g', alpha=0.2)
+            plt.axvline(x=r[0], color='g', alpha=0.1)
         if r[3]:
-            plt.axvline(x=r[0], color='r', alpha=0.2)
+            plt.axvline(x=r[0], color='r', alpha=0.1)
         if r[4]:
-            plt.axvline(x=r[0], color='lightyellow', alpha=0.4)
+            plt.axvline(x=r[0], color='gold', alpha=0.1)
     plt.draw()
     return plt
 
@@ -54,6 +62,7 @@ try:
     df['date_time'] = df['date_time'].astype('datetime64[ns]')  # correct date_time type definition
     df.rename(columns={'Unnamed: 0':'ix'}, inplace=True)
     y = df.iloc[:, [0, 2, 4, 5, 6]].values
+    print(y[0])
     print("Finished opening file, Y has dimensions: " + str(df.shape) + "\n" + str(df.keys())+ "\n")
 except Exception as ex:
     print("Something went wrong when reading df from file, error code: " + str(ex))
@@ -61,7 +70,7 @@ except Exception as ex:
     exit()
 
 y = np.c_[y, np.zeros(y.shape[0], dtype=float)]   # create column for bank
-
+bank_start = bank
 # Start trading
 while not sim_finished:
     trading_l = False
@@ -69,52 +78,62 @@ while not sim_finished:
     for i in range(0, len(y)-1, 1):
         sg = y[i, 1] # current spot gold
         # long
-        if y[i, 3]:
-            if not trading_l:
+        if y[i, 2]:
+            if not trading_l and not trading_s:
                 # 1 Contract cost
                 m1 = (sg+spread)*0.057
                 if bank/m1 > std_pos:   # if enough money to open standard position
                     position = std_pos  # else as many as possible, break if 0
                 else:
                     position = int(bank/m1)
-                    if position == 0: break
+                    if position == 0:
+                        print("bankrupt")
+                        break
                 l_start = sg
                 trading_l = True
+                # print("long at index: " + str(i) + ", long signal: " + str(y[i, 2]))
             target = y[i, 1] * goal   # Update target
 
         # short
-        if y[i, 4]:
-            if not trading_s:
+        if y[i, 3]:
+            if not trading_s and not trading_l:
                 # 1 Contract cost
                 m1 = (sg-spread)*0.057
                 if bank/m1 > std_pos:   # if enough money to open standard position
                     position = std_pos  # else as many as possible, break if 0
                 else:
                     position = int(bank/m1)
-                    if position == 0: break
+                    if position == 0:
+                        print("bankrupt")
+                        break
                 s_start = sg
                 trading_s = True
+                # print("short at index: " + str(i) + ", short signal: " + str(y[i, 3]))
             target = y[i, 1] * (1/goal)  # Update target
 
         # hold or end position
         if trading_l:   # end long
-            if y[i, 1] >= target:
+            if (y[i, 1] >= target or y[i, 3]):  # holdingn not benifitial when long (2015, 2014 tested)
                 position = 0
-                bank += std_pos*(l_start-sg) - (std_pos*spread)  # Trade gain EUR
+                bank += std_pos*(sg-l_start) - (std_pos*spread)  # Trade gain EUR
                 trading_l = False
                 target = 0
+                if (std_pos*(sg-l_start) - (std_pos*spread)) < 0:
+                    print("----Closed long at index: " + str(i) + ", gain: " + str(std_pos*(sg-l_start) - (std_pos*spread)))
         if trading_s:  # end short
-            if y[i, 1] <= target:
+            if (y[i, 1] <= target or y[i, 2]) and not y[i, 4]:  # holding is benifital when short (2014, 2015 tested)
                 position = 0
-                bank += std_pos*(sg-s_start) - (std_pos*spread)  # Trade gain EUR
+                bank += std_pos*(s_start-sg) - (std_pos*spread)  # Trade gain EUR
                 trading_s = False
                 target = 0
+                if (std_pos*(s_start-sg) - (std_pos*spread)) < 0:
+                    print("----Closed short at index: " + str(i) + ", gain: " + str(std_pos*(s_start-sg) - (std_pos*spread)))
 
         # logging
         y[i, 5] = bank
 
     sim_finished = True
-print("Simulation completed \n")
+print("\nSimulation completed, bank: " + str(round(bank)) + ", gain: " + str(round(bank-bank_start)) + ", " + str(round(bank/bank_start*100)) + "% \n")
 plot = plot_result(y)
 plot.show()
 exit()
